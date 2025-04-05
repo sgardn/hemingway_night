50d5d335b21a580eaa68809b5d5a385019e188be
Scott Gardner <sgardn04@gmail.com>
2025-04-05 11:17:52 -0400

    feat: Add phoenix_storybook dependency and update phoenix_live_view

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule HemingwayNight.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.20.2"},
+      {:phoenix_live_view, "~> 1.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
@@ -59,7 +59,8 @@ defmodule HemingwayNight.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
       {:redix, "~> 1.1"},
-      {:poolboy, "~> 1.5"}
+      {:poolboy, "~> 1.5"},
+      {:phoenix_storybook, "~> 0.8.1"}
     ]
   end
 
